36d2e6935170a66d9059615dc28f9e2ea765fb9d
nilser@gmail.com <nilser@gmail.com>
2020-03-17 23:17:38 +0100

    Add command for import all

--- src/descriptors/descriptors.ts
+++ src/descriptors/descriptors.ts
@@ -10,13 +10,13 @@ const getAwsData = <T>(gen: (callback: AWSCallback<T>) => void) =>
 
 const getResult = <T>(result: AWSResult<T>) => {
   if (!result.data) {
-    throw result.err;
+    return undefined;
   }
   return result.data;
 };
 
 interface ResourceDescriptor {
-  name: string | undefined;
+  identifier: string | undefined;
 }
 
 interface ImportType<T> {
@@ -27,7 +27,7 @@ interface ImportType<T> {
   doImport: (identifier: string) => Promise<ImportResult[]>;
 }
 
-interface ImportResult {
+export interface ImportResult {
   name: string;
   resource: ResourceElement;
 }
@@ -37,12 +37,23 @@ export const descriptors: ImportType<any>[] = [
     type: "aws_api_gateway_rest_api",
     fetcher: async () => {
       const result = await getAwsData<AWS.APIGateway.RestApis>(cb => new AWS.APIGateway().getRestApis({}, cb));
-      return result.items || [];
+      return result?.items || [];
     },
     matcher: (api: AWS.APIGateway.RestApi, instance) => api.name === instance.attributes.name,
-    descriptor: (api: AWS.APIGateway.RestApi) => ({ name: api.name }),
-    doImport: async () => {
-      throw new Error("Not implemented");
+    descriptor: (api: AWS.APIGateway.RestApi) => ({ identifier: api.id }),
+    doImport: async identifier => {
+      const result = await getAwsData<AWS.APIGateway.RestApi>(cb =>
+        new AWS.APIGateway().getRestApi({ restApiId: identifier }, cb),
+      );
+      return [
+        {
+          name: identifier,
+          resource: resource("aws_api_gateway_rest_api", identifier, {
+            name: result?.name,
+            description: result?.description,
+          }),
+        },
+      ];
     },
   },
   {
@@ -51,45 +62,55 @@ export const descriptors: ImportType<any>[] = [
       const result = await getAwsData<AWS.Route53.ListHostedZonesResponse>(cb =>
         new AWS.Route53().listHostedZones({}, cb),
       );
-      return result.HostedZones || [];
+      return result?.HostedZones || [];
     },
     matcher: (zone: AWS.Route53.HostedZone, instance) => zone.Name === instance.attributes.name,
-    descriptor: (zone: AWS.Route53.HostedZone) => ({ name: zone.Name }),
-    doImport: async () => {
-      throw new Error("Not implemented");
+    descriptor: (zone: AWS.Route53.HostedZone) => ({ identifier: zone.Name }),
+    doImport: async identifier => {
+      return [{ name: identifier, resource: resource("aws_route53_zone", identifier, {}) }];
     },
   },
   {
     type: "aws_s3_bucket",
     fetcher: async () => {
       const result = await getAwsData<AWS.S3.ListBucketsOutput>(cb => new AWS.S3().listBuckets(cb));
-      return result.Buckets || [];
+      return result?.Buckets || [];
     },
     matcher: (bucket: AWS.S3.Bucket, instance) => bucket.Name === instance.attributes.id,
-    descriptor: (bucket: AWS.S3.Bucket) => ({ name: bucket.Name }),
+    descriptor: (bucket: AWS.S3.Bucket) => ({ identifier: bucket.Name }),
     doImport: async (identifier: string) => {
-      const result = await getAwsData<AWS.S3.ListBucketsOutput>(cb => new AWS.S3().getBucketAcl(cb));
-      return [];
+      const acl = await getAwsData<AWS.S3.GetBucketAclOutput>(cb =>
+        new AWS.S3().getBucketAcl({ Bucket: identifier }, cb),
+      );
+      const web = await getAwsData<AWS.S3.GetBucketWebsiteOutput>(cb =>
+        new AWS.S3().getBucketWebsite({ Bucket: identifier }, cb),
+      );
+      console.log({ identifier, acl, web });
+      return [{ name: identifier, resource: resource("aws_s3_bucket", identifier, {}) }];
     },
   },
   {
     type: "aws_iam_role",
     fetcher: async () => {
       const result = await getAwsData<AWS.IAM.ListRolesResponse>(cb => new AWS.IAM().listRoles(cb));
-      return result.Roles || [];
+      return result?.Roles || [];
     },
     matcher: (role: AWS.IAM.Role, instance) => role.RoleName === instance.attributes.id,
-    descriptor: (role: AWS.IAM.Role) => ({ name: role.RoleName }),
+    descriptor: (role: AWS.IAM.Role) => ({ identifier: role.RoleName }),
     doImport: async (identifier: string) => {
       const result = await getAwsData<AWS.IAM.GetRoleResponse>(cb =>
         new AWS.IAM().getRole({ RoleName: identifier }, cb),
       );
+      if (!result) {
+        throw new Error();
+      }
       const role = result.Role;
       return [
         {
           name: role.RoleName,
           resource: resource("aws_iam_role", role.RoleName, {
             description: role.Description || "",
+            path: role.Path,
             assume_role_policy:
               role.AssumeRolePolicyDocument &&
               jsonencode(JSON.parse(decodeURIComponent(role.AssumeRolePolicyDocument))),
--- src/terraform-import.ts
+++ src/terraform-import.ts
@@ -1,17 +1,36 @@
 #!/usr/bin/env node
 
-import { composite, createFileContent } from "terraform-state-in-typescript";
+import { composite, createFileContent, resource, ResourceElement } from "terraform-state-in-typescript";
 import { exec } from "child_process";
 import * as AWS from "aws-sdk";
 import * as fs from "fs";
 import chalk from "chalk";
-import { TerraformState } from "./terraform-state";
+import { TerraformState, TerraformResource } from "./terraform-state";
 import * as yargs from "yargs";
-import { descriptors } from "./descriptors/descriptors";
+import { descriptors, ImportResult } from "./descriptors/descriptors";
 
-const doList = async () => {
+interface ResourceInfo {
+  type: string;
+  identifier: string | undefined;
+}
+
+const asyncForEach = async <T>(arr: T[], fn: (t: T) => Promise<void>) => {
+  for (let i = 0; i < arr.length; i++) {
+    await fn(arr[i]);
+  }
+};
+
+const asyncReduce = async <Aggr, T>(arr: T[], fn: (ag: Aggr, t: T) => Promise<Aggr>, a0: Aggr) => {
+  let a = a0;
+  await asyncForEach(arr, async elem => {
+    a = await fn(a, elem);
+  });
+  return a;
+};
+
+const getResources = async (): Promise<ResourceInfo[]> => {
   const state: TerraformState = JSON.parse(fs.readFileSync("./terraform.tfstate").toString());
-  let numNewResources = 0;
+  const resources: ResourceInfo[] = [];
   for (let i = 0; i < descriptors.length; i++) {
     const { type, fetcher, descriptor, matcher } = descriptors[i];
     const items = await fetcher();
@@ -21,13 +40,63 @@ const doList = async () => {
         .filter(r => r.type === type)
         .find(r => r.instances.some(i => matcher(item, i)));
       if (existingResource) {
-        console.log(chalk.bgGray(`${type}/${description.name}`));
       } else {
-        console.log(chalk.green(`${type}/${description.name}`));
-        numNewResources++;
+        resources.push({ type, identifier: description.identifier });
       }
     });
   }
+  return resources;
+};
+
+const importResources = async (resources: ResourceInfo[]) => {
+  const imports = await asyncReduce<ImportResult[], ResourceInfo>(
+    resources,
+    async (allResources: ImportResult[], r: ResourceInfo) => {
+      const descriptor = descriptors.find(d => d.type === r.type);
+      if (!descriptor) {
+        throw new Error("Illegal type: " + r.type);
+      }
+      if (!r.identifier) {
+        throw new Error("Illegal identifier: " + r.identifier);
+      }
+      console.log(chalk.yellow(`Importing ${r.type} ${r.identifier}...`));
+      const result = await descriptor.doImport(r.identifier);
+      return [...allResources, ...result];
+    },
+    [],
+  );
+
+  const getFile = (r: ResourceElement) => `./${r.resourceType} ${r.resourceId}.tf`;
+
+  imports.forEach(v => {
+    fs.writeFileSync(getFile(v.resource), createFileContent(v.resource));
+  });
+
+  await asyncForEach(imports, v =>
+    new Promise((resolve, reject) =>
+      exec(`terraform import ${v.resource.resourceType}.${v.resource.resourceId} ${v.name}`, (err, data) => {
+        if (err) {
+          fs.unlinkSync(getFile(v.resource));
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      }),
+    )
+      .then(() => {})
+      .catch(err => {
+        console.error(err);
+        console.log(chalk.red(`Resource could not be imported: ${v.resource.resourceType}.${v.resource.resourceId}`));
+      }),
+  );
+};
+
+const doList = async () => {
+  const resources = await getResources();
+  resources.forEach(resource => {
+    console.log(chalk.green(`${resource.type}/${resource.identifier}`));
+  });
+  const numNewResources = resources.length;
   if (numNewResources === 0) {
     console.log("Found 0 new resources to add.");
   } else {
@@ -39,33 +108,37 @@ const doImport = async (url: string) => {
   const [type, ...rest] = url.split("/");
   const identifier = rest.join("/");
 
-  const descriptor = descriptors.find(d => d.type === type);
-  if (!descriptor) {
-    throw new Error("Illegal type: " + type);
-  }
-  console.log(chalk.yellow(`Importing ${type} ${identifier}...`));
-  const resources = await descriptor.doImport(identifier);
-  fs.writeFileSync("./imported.tf", createFileContent(composite(...resources.map(r => r.resource))));
+  await importResources([{ type, identifier }]);
+};
 
-  resources.forEach(async v => {
-    await new Promise((resolve, reject) =>
-      exec(`terraform import ${v.resource.resourceType}.${v.resource.resourceId} ${v.name}`, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      }),
-    ).catch(err =>
-      console.log(chalk.red(`Resource could not be imported: ${v.resource.type}.${v.resource.resourceId}`)),
-    );
-  });
+const doImportAll = async () => {
+  const resources = await getResources();
+  await importResources(resources);
 };
 
 AWS.config.update({ region: "eu-west-1" });
+
 yargs
   .scriptName("terraform-import")
   .usage("$0 <cmd> [args]")
+  .command(
+    "list",
+    "List all remote resoruces",
+    yargs => {
+      yargs
+        .positional("resource", {
+          type: "string",
+          demandOption: true,
+          describe: "the identifier for the existing resource (for example aws_s3_bucket/mybucket)",
+        })
+        .positional("id", {
+          type: "string",
+          demandOption: false,
+          describe: "the id for the created managed resource",
+        });
+    },
+    () => doList(),
+  )
   .command(
     "import [resource] [id]",
     "Imports a remote resource to your Terraform state",
@@ -87,22 +160,12 @@ yargs
     },
   )
   .command(
-    "list",
-    "List all remote resoruces",
-    yargs => {
-      yargs
-        .positional("resource", {
-          type: "string",
-          demandOption: true,
-          describe: "the identifier for the existing resource (for example aws_s3_bucket/mybucket)",
-        })
-        .positional("id", {
-          type: "string",
-          demandOption: false,
-          describe: "the id for the created managed resource",
-        });
+    "import-all",
+    "Imports all resources to terraform",
+    yargs => {},
+    async argv => {
+      doImportAll();
     },
-    () => doList(),
   )
   .strict()
   .help().argv;
